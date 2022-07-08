16612dbcd1fc71f795a53e3e023b903557233d3c
Petr Mazka <petr.mazka@gmail.com>
2022-07-08 15:32:04 +0300

    FT-10 update
    Добавил эффект на модалку, ссылку на гит

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -1,13 +1,16 @@
+import confetti from 'canvas-confetti';
 import { refs } from './refs/refs';
 
 let modalTeamRef;
 modalTeamRef = refs().modalTeamRef;
+const colors = ['#ff6b02', '#ff001b', '#ffffff'];
 
 modalTeamRef.teamModalOpen.addEventListener('click', openModal);
 
 function openModal(e) {
   e.preventDefault();
   modalTeamRef.teamModal.classList.remove('is-hidden');
+  showConfetti();
 
   window.addEventListener('keydown', closeModalByEsc);
   window.addEventListener('click', closeModalByClick);
@@ -36,5 +39,12 @@ function openModal(e) {
   function closeModal(e) {
     modalTeamRef.teamModal.classList.add('is-hidden');
     clearEventListeners();
+  }
+
+  function showConfetti() {
+    confetti.create(document.getElementById('canvas'), {
+      resize: true,
+      useWorker: true,
+    })({ particleCount: 200, spread: 200, zIndex: 2021, colors: colors });
   }
 }
